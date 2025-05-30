a36de536352b46768ec93b723bcd81df4bfdea6f
SiddharthSA2003 <siddharthanvekar7777@gmail.com>
2025-05-31 00:33:14 +0530

    Update serial_modulo.v

--- rtl/serial_modulo.v
+++ rtl/serial_modulo.v
@@ -1,71 +1,96 @@
 `timescale 1ns / 1ps
 
-module serial_modulo #( 
-    parameter WIDE_IN  = 512,    // Width of input A
-    parameter WIDE_MOD = 255     // Width of modulus P
-)(
-    input  wire                 clk,
-    input  wire                 reset,
-    input  wire                 start,
-    input  wire [WIDE_IN-1:0]   A,
-    output reg  [WIDE_MOD-1:0]  result,
-    output reg                  done
-);
-    
-    localparam  Prime = 255'h7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFED;
+module serial_modulo(
+    input wire          clk,
+    input wire          reset,
+    input wire          start,
+    input wire [511:0]  A,        // 512-bit input
+    output reg [254:0]  result,   // 255-bit reduced output
+    output reg          done
+    );
 
-    reg  [WIDE_IN-1: 0] temp;
-    reg  [8:0]          bit_index;  // Can count up to 512
+    // Curve25519 modulus: p = 2^255 - 19
+    localparam P1 = 255'h7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFED;
+    localparam P2 = 256'hFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFDA;
+    localparam P3 = 257'h1FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFB4;
+    localparam P4 = 258'h3FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF68;
+    localparam P5 = 259'h7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFED0;
+    localparam P6 = 260'hFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFDA0;
+    localparam P7 = 261'h1FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFB40;
     
-    wire [WIDE_MOD:0]   temp_slice;
+    localparam  IDLE = 2'b00,
+                COMP = 2'b01,
+                ADD  = 2'b10,
+                DONE = 2'b11;
     
-    localparam  IDLE        = 2'b00,
-                SUBTRACT    = 2'b01,
-                FINALIZE    = 2'b10;
-                
-    reg [1:0] state;
-    
-    assign temp_slice = temp[bit_index -: WIDE_MOD + 1];
+    reg  [1:0]   state;
+    reg  [262:0] A_temp;
 
-    always @(posedge clk or posedge reset) begin
+    reg  [254:0] A_low;
+    reg  [260:0] A_high;
+    
+    reg  [262:0] sum;
+    
+    always @ (posedge clk)
+    begin
         if (reset) begin
-            temp        <= 0;
-            bit_index   <= 0;
-            result      <= 0;
-            done        <= 0;
-            state       <= IDLE;
+            A_temp  <= 0;
+            A_low   <= 0;
+            A_high  <= 0;
+            
+            sum     <= 0;
+            result  <= 0;
+            done    <= 0;
+            state   <= IDLE;
         end
         else begin
             case (state)
-                IDLE    : begin
-                              done  <= 0;
-                              
-                              if (start) begin
-                                  temp          <= A;
-                                  bit_index     <= WIDE_IN - 1;
-                                  state         <= SUBTRACT;
-                              end
-                          end
-                SUBTRACT: begin
-                              if (bit_index >= WIDE_MOD) begin
-                                  if (temp[bit_index] == 1) begin
-                                      temp[bit_index -: WIDE_MOD + 1] <= temp_slice - Prime;
-                                  end
-                                  bit_index <= bit_index - 1;
-                              end
-                              else
-                                  state <= FINALIZE;
-                          end
-                FINALIZE: begin
-                              if (temp[WIDE_MOD:0] >= Prime)
-                                  result <= temp[WIDE_MOD-1:0] - Prime;
-                              else
-                                  result <= temp[WIDE_MOD-1:0];
-                              
-                              done  <= 1;
-                              
-                              state <= IDLE;
-                          end
+                IDLE  : begin
+                            done    <= 0;
+                            
+                            if (start) begin
+                                A_low   <= A[254:0];
+                                A_high  <= A[511:255];
+                                A_temp  <= 0;
+                                state   <= COMP;
+                            end
+                        end
+                COMP  : begin
+                            A_temp  <= (A_high << 4) + (A_high << 1) + A_high;
+                            state   <= ADD;
+                        end
+                ADD   : begin
+                            sum     <= A_low + A_temp;
+                            state   <= DONE;
+                        end
+                DONE  : begin
+                            if (sum[262] == 1) begin
+                                sum  <= sum - P7;
+                            end
+                            else if (sum[261] == 1) begin
+                                sum  <= sum - P6;
+                            end
+                            else if (sum[260] == 1) begin
+                                sum  <= sum - P5;
+                            end
+                            else if (sum[259] == 1) begin
+                                sum  <= sum - P4;
+                            end
+                            else if (sum[258] == 1) begin
+                                sum  <= sum - P3;
+                            end
+                            else if (sum[257] == 1) begin
+                                sum  <= sum - P2;
+                            end
+                            else if (sum >= P1) begin
+                                sum  <= sum - P1;
+                            end
+                            else begin
+                                result  <= sum;
+                                done    <= 1;
+                                state   <= IDLE;
+                            end
+                        end
             endcase
         end
     end
